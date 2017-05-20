cc6df9612fa436f3ff6fd74d64783f73f0be74bb
“Trinidad <tfuentesfuentealba@gmail.com>
2017-05-20 01:33:17 -0300

    agregando eventos js

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+var ins = document.getElementById("inscribete");
+ins.addEventListener("click", function(){
+	alert("Chicas que cambian el Mundo")
+});
+
+
+var hack = document.getElementById("hack-btn");
+hack.addEventListener("click", function(){
+	document.getElementById("cambiar").style.float="right";
+})
